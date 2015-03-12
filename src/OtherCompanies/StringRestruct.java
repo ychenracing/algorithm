5f48b9125f668641fe53df89da97ca1ffafa79ef
ychen <racing@ychen.local>
2015-03-12 16:27:09 +0800

    add another method to StringRestruct

--- src/OtherCompanies/StringRestruct.java
+++ src/OtherCompanies/StringRestruct.java
@@ -13,9 +13,20 @@ import java.util.Scanner;
 public class StringRestruct {
 
 	public static void main(String[] args) {
+		StringRestruct sr = new StringRestruct();
+		sr.method1();
+		sr.method2();
+	}
+	
+	/*
+	 * <pre>time complexity: {@code O(n)}
+	 * space complexity: {@code O(1)}</pre>
+	 * 
+	 */
+	public void method1(){
 		Scanner scanner = new Scanner(System.in);
 		char[] inputString = null;
-		while(!(inputString = scanner.nextLine().trim().toCharArray()).equals("exit")){
+		while(!(inputString = scanner.nextLine().toCharArray()).equals("exit")){
 			int end = inputString.length - 1, start = end - 1;
 			while(start >= 0 && end >0){
 				while(end > 0 && inputString[end] != '*')
@@ -34,8 +45,29 @@ public class StringRestruct {
 			System.out.println(inputString);
 		}
 		scanner.close();
-		
-		
 	}
-
+	
+	/*
+	 * <pre>time complexity: {@code O(n)}
+	 * space complexity: {@code O(n)}</pre>
+	 * 
+	 */
+	public void method2(){
+		StringBuilder sbstars = new StringBuilder();
+		StringBuilder sbchars = new StringBuilder();
+		Scanner scanner = new Scanner(System.in);
+		String inputString = null;
+		while(!(inputString = scanner.nextLine()).trim().equals("exit")){
+			int iter = 0;
+			while(iter < inputString.length()){
+				if(inputString.charAt(iter) == '*')
+					sbstars.append(inputString.charAt(iter));
+				else sbchars.append(inputString.charAt(iter));
+				iter++;
+			}
+			sbstars.append(sbchars);
+			System.out.println(sbstars);
+		}
+		scanner.close();
+	}
 }
