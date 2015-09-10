7f7d142d806b4037d1b270095e88ce86bceffa2a
ychenracing <ychenracing@gmail.com>
2015-09-10 20:17:15 +0800

    add meituan

---
+++ src/OtherCompanies/TrimAndCompact.java
@@ -0,0 +1,35 @@
+package OtherCompanies;
+
+public class TrimAndCompact {
+
+    public static void main(String[] args) {
+        System.out.println(transform(" i       love   meituan "));
+        System.out.println(simpleTransform(" i       love   meituan "));
+    }
+
+    public static String transform(String canString) {
+        char[] canChars = canString.toCharArray();
+        int index = 0, begin = 0;
+        while (canChars[index] == ' ')
+            index++;
+
+        while (index < canChars.length) {
+            while (index < canChars.length && canChars[index] != ' ') {
+                canChars[begin++] = canChars[index++];
+            }
+            while (index < canChars.length && canChars[index] == ' ') {
+                index++;
+            }
+            if (index < canChars.length) {
+                canChars[begin++] = ' ';
+            }
+        }
+
+        return new String(canChars, 0, begin);
+    }
+
+    public static String simpleTransform(String canString) {
+        return canString.trim().replaceAll("\\s+", " ");
+    }
+
+}
