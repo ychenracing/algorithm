befe9e568e945d7a94f70b6c7bb9f578152570f1
ychenracing <ychenracing@gmail.com>
2015-10-08 17:36:27 +0800

    add 最长递增子序列

--- src/ctci/chapter11/Question11_7.java
+++ src/ctci/chapter11/Question11_7.java
@@ -2,10 +2,9 @@ package ctci.chapter11;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 /**
- * 叠罗汉
+ * 叠罗汉 & 最长递增子序列
  * @author racing
  * @version $Id: Question11_7.java, v 0.1 Oct 7, 2015 1:02:44 PM racing Exp $
  */
@@ -41,27 +40,34 @@ public class Question11_7 {
     }
 
     public static void main(String[] args) {
-        Person[] persons = new Person[8];
-        persons[0] = new Person(65, 100);
-        persons[1] = new Person(70, 150);
-        persons[2] = new Person(56, 90);
-        persons[3] = new Person(75, 190);
-        persons[4] = new Person(60, 95);
-        persons[5] = new Person(68, 110);
-        persons[6] = new Person(30, 200);
-        persons[7] = new Person(60, 110);
+        //        ArrayList<Person> array = new ArrayList<Person>();
+        //        array.add(new Person(65, 100));
+        //        array.add(new Person(70, 150));
+        //        array.add(new Person(56, 90));
+        //        array.add(new Person(75, 190));
+        //        array.add(new Person(60, 95));
+        //        array.add(new Person(68, 110));
+        //        array.add(new Person(30, 200));
+        //        array.add(new Person(60, 110));
+        //        Collections.sort(array);
+        //
+        //        ArrayList<Person>[] solutions = new ArrayList[array.size()];
+        //        getLayers(array, solutions, 0);
+        //        ArrayList<Person> bestSequence = null;
+        //        for (int i = 0; i < array.size(); i++) {
+        //            bestSequence = sequenceWithMaxLength(bestSequence, solutions[i]);
+        //        }
+        //
+        //        System.out.println(bestSequence);
 
-        ArrayList<Person> array = new ArrayList<Person>(Arrays.asList(persons));
-        Collections.sort(array);
-
-        ArrayList<Person>[] solutions = new ArrayList[array.size()];
-        getLayers(array, solutions, 0);
-        ArrayList<Person> bestSequence = null;
-        for (int i = 0; i < persons.length; i++) {
-            bestSequence = sequenceWithMaxLength(bestSequence, solutions[i]);
+        int[] nums = { 5, 6, 7, 1, 2, 8 };
+        int[][] result = new int[nums.length][];
+        getMaxSubSequence(nums, result, 0);
+        int[] maxLength = new int[0];
+        for (int[] array : result) {
+            maxLength = maxLength.length > array.length ? maxLength : array;
         }
-
-        System.out.println(bestSequence);
+        System.out.println(Arrays.toString(maxLength));
 
     }
 
@@ -91,5 +97,22 @@ public class Question11_7 {
         if (seq2 == null)
             return seq1;
         return seq1.size() > seq2.size() ? seq1 : seq2;
+    }
+
+    public static void getMaxSubSequence(int[] nums, int[][] result, int currentIndex) {
+        if (currentIndex >= nums.length)
+            return;
+        int[] preMax = new int[0];
+        for (int i = 0; i < currentIndex; i++) {
+            if (nums[i] < nums[currentIndex])
+                preMax = preMax.length > result[i].length ? preMax : result[i];
+        }
+
+        int[] newMax = new int[preMax.length + 1];
+        System.arraycopy(preMax, 0, newMax, 0, preMax.length);
+        newMax[newMax.length - 1] = nums[currentIndex];
+        result[currentIndex] = newMax;
+
+        getMaxSubSequence(nums, result, currentIndex + 1);
     }
 }
