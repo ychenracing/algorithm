fe800fee29f2543a9f18e9a0a162d695877abb5f
ychenracing <ychenracing@gmail.com>
2015-10-12 20:16:15 +0800

    AC

--- src/leetcode/insertionsortlist/Solution.java
+++ src/leetcode/insertionsortlist/Solution.java
@@ -4,6 +4,9 @@
  */
 package leetcode.insertionsortlist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <pre>
  * Sort a linked list using insertion sort.
@@ -23,6 +26,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
 
     }
 
