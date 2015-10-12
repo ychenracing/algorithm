988e4d5fc6334233558ec9d7a72dfe912c163254
ychenracing <ychenracing@gmail.com>
2015-10-12 20:37:16 +0800

    create

---
+++ src/leetcode/model/ListNode.java
@@ -0,0 +1,11 @@
+package leetcode.model;
+
+public class ListNode {
+
+    int      val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/leetcode/model/TreeNode.java
@@ -0,0 +1,12 @@
+package leetcode.model;
+
+public class TreeNode {
+
+    int      val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
